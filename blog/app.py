170ecf1a88f0183f0b01ab95f6a9f31df932318b
Nathan3-14 <nathan.watson.172@gmail.com>
2024-08-09 12:36:02 +0100

    Added admin Added post creation

--- blog/app.py
+++ blog/app.py
@@ -1,3 +1,4 @@
+from typing import Any, List
 from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -24,9 +25,30 @@ def index():
 @app.route("/posts/create/", methods=["POST", "GET"])
 def post_create():
     if request.method == "POST":
-        pass
+        try:
+            post_title = request.form["title"]
+            post_body = request.form["body"]
+            new_post = Post(title=post_title, body=post_body) # type:ignore
+            db.session.add(new_post)
+            db.session.commit()
+        except:
+            return "<div><style>color: red</style><h1>An error occurred while creating your post</h1></div>"
+        return redirect("/")
     else:
         return render_template("create-post.html")
+
+@app.route("/admin/")
+def admin():
+    offset = request.args.get("offset")
+    if offset == None or not offset.isdigit():
+        offset = 0
+    else:
+        offset = int(offset)
+    return render_template("admin.html", posts=Post.query.order_by(Post.date_created).all()[0+offset:10+offset])
+
+@app.route("/admin/<int:id>")
+def admin_post(id: int):
+    return render_template("admin_post.html", post=Post.query.get(id))
 
 
 if __name__ == "__main__":
@@ -34,5 +56,17 @@ if __name__ == "__main__":
 
 #* Import app to create db *#
 if __name__ == "app":
-    with app.app_context():
+    def app_context(func):                                                                                            
+        def inner(args: List[Any]):
+            with app.app_context():
+                func(*args)
+        return inner  
+       
+    @app_context
+    def remove_post(post_id: int):
+        db.session.delete(Post.query.get(post_id))
+        db.session.commit()
+    
+    @app_context
+    def create_all():
         db.create_all()
