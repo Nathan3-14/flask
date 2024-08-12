064c7c0b370115b9f7bceb7a7996e217cfb90891
Nathan3-14 <nathan.watson.172@gmail.com>
2024-08-12 21:39:42 +0100

    Fixed user login

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, List, Tuple
 from flask import Flask, make_response, redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -9,16 +9,40 @@ except ModuleNotFoundError:
     quit()
 
 
+def get_form_property(property: str, template_path: str) -> Tuple[bool, str]: #? (is_template, value)
+    value = request.form[property]
+    if value == "":
+        return (True, render_template(template_path, needed_fields=[property]))
+    return (False, value)
+
+
 def set_cookie(cookie_name: str, value: Any, redirect_location: str="/"):
     response = make_response(redirect(redirect_location))
     response.set_cookie(cookie_name, value)
    
     return response
 
+
 def get_cookie(cookie_name: str):
    value = request.cookies.get(cookie_name)
    return value
 
+
+def render_template_full(notifications: List[str], *args, **kwargs) -> str:
+    notification_string = ""
+    for notification in notifications:
+        notification_string += f"{notification}"
+    
+    current_user = get_current_user()
+    if current_user == None:
+        return render_template(*args, **kwargs, notifications=notification_string, no_user=True)
+
+    return render_template(*args, **kwargs, notifications=notification_string, current_user=current_user.display_name)
+
+
+def get_current_user() -> "User | None":
+    current_user = User.query.filter_by(uuid=get_cookie("current_user")).first()
+    return current_user
 
 
 app = Flask(__name__)
@@ -28,8 +52,13 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(24))
     display_name = db.Column(db.String(20), nullable=False)
     password = db.Column(db.Integer, nullable=False)
+    permission_level = db.Column(db.Integer, default=0, nullable=False)
+
+    def set_uuid(self):
+        self.uuid = f"{self.display_name}{self.id}"
 
     def __repr__(self) -> str:
         return f"User {self.id} ({self.display_name})"
@@ -56,7 +85,28 @@ class Post(db.Model):
 @app.route("/")
 def index():
     top_posts = Post.query.order_by(Post.date_created).all()[:5]
-    return render_template("home.html", top_posts=top_posts, current_user=get_cookie("current_user"))
+    return render_template_full([], "home.html", top_posts=top_posts)
+
+@app.route("/users/login/", methods=["POST", "GET"])
+def user_login():
+    if request.method == "POST":
+        try:
+            uuid = get_form_property("uuid", "login-user.html")
+            if uuid[0]: return uuid[1]
+            uuid = uuid[1]
+            password = get_form_property("password", "login-user.html")
+            if password[0]: return password[1]
+            password = password[1]
+
+            current_user = User.query.filter_by(uuid=uuid).first()
+            if current_user == None:
+                return render_template_full([], "login-user.html", needed_fields=["uuid", "password"], failed=True)
+            return set_cookie("current_user", current_user.uuid)
+        except Exception as e:
+            print(e)
+            return "<div><style>color: red</style><h1>An error occurred while creating your user</h1></div>"
+    else:
+        return render_template_full([], "login-user.html", needed_fields=[])
 
 @app.route("/users/create/", methods=["POST", "GET"])
 def user_create():
@@ -64,22 +114,24 @@ def user_create():
         try:
             user_name = request.form["username"]
             if user_name == "":
-                return render_template("create-user.html", needed_fields=["username"])
+                return render_template_full([], "create-user.html", needed_fields=["username"])
             user_password = request.form["password"]
             if user_password == "":
-                return render_template("create-user.html", needed_fields=["password"])
+                return render_template_full([], "create-user.html", needed_fields=["password"])
             
             new_user = User(display_name=user_name, password=encr(user_password)) # #type:ignore
             db.session.add(new_user)
             db.session.commit()
+            new_user.set_uuid()
+            db.session.commit()
 
-            return set_cookie("current_user", new_user.display_name)
+            return set_cookie("current_user", new_user.uuid)
         except Exception as e:
             print(e)
             return "<div><style>color: red</style><h1>An error occurred while creating your user</h1></div>"
         
     else:
-        return render_template("create-user.html", needed_fields=[])
+        return render_template_full([], "create-user.html", needed_fields=[])
 
 @app.route("/posts/create/", methods=["POST", "GET"])
 def post_create():
@@ -87,17 +139,18 @@ def post_create():
         try:
             post_title = request.form["title"]
             if post_title == "":
-                return render_template("create-post.html", needed_fields=["title"])
+                return render_template_full([], "create-post.html", needed_fields=["title"])
             
             post_body = request.form["body"]
             new_post = Post(title=post_title, body=post_body) # type:ignore
             db.session.add(new_post)
             db.session.commit()
-        except:
+        except Exception as e:
+            print(e)
             return "<div><style>color: red</style><h1>An error occurred while creating your post</h1></div>"
         return redirect("/")
     else:
-        return render_template("create-post.html", needed_fields=[])
+        return render_template_full([], "create-post.html", needed_fields=[])
 
 @app.route("/posts/<int:id>/")
 def view_post(id: int):
@@ -106,6 +159,10 @@ def view_post(id: int):
 
 @app.route("/admin/")
 def admin():
+    if get_current_user().permission_level != 2:
+        return redirect("/")
+
+
     page = request.args.get("page")
     per_page = request.args.get("perpage")
     if page == None or not page.isdigit():
@@ -122,14 +179,15 @@ def admin():
         page*per_page:
         (page*per_page)+per_page
     ]
-    return render_template("admin.html", posts=posts, page_count=pages, per_page=per_page)
+    return render_template_full([], "admin.html", posts=posts, page_count=pages, per_page=per_page)
 
 @app.route("/admin/delete/<int:id>/")
 def admin_delete_post(id: int):
     try:
-        db.session.delete(session.get(id))
+        db.session.delete(Post.query.get(id))
         db.session.commit()
-    except:
+    except Exception as e:
+        print(e)
         return "<div><style>color: red</style><h1>An error occurred while deleting the post</h1></div>"
     return redirect("/admin")
 
@@ -139,17 +197,23 @@ if __name__ == "__main__":
 
 #* Import app to create db *#
 if __name__ == "app":
-    def app_context(func):                                                                                            
-        def inner(args: List[Any]=[]):
-            with app.app_context():
-                func(*args)
-        return inner  
-       
-    @app_context
-    def remove_post(post_id: int):
-        db.session.delete(session.get(post_id))
-        db.session.commit()
+    def delete_user(id: int, all: bool=False):
+        with app.app_context():
+            if all:
+                for user in User.query.order_by("id").all():
+                    db.session.delete(user)
+            else:
+                db.session.delete(User.query.get(id))
+            db.session.commit()
     
-    @app_context
     def create_all():
-        db.create_all()
+        with app.app_context():
+            db.create_all()
+    
+    def add_admin(uname: str, pword: str):
+        with app.app_context():
+            new_user = User(display_name=uname, password=encr(pword), permission_level=2)
+            db.session.add(new_user)
+            db.session.commit()
+            new_user.set_uuid()
+            db.session.commit()
