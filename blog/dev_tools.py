68afd638edb1eccbe193234bf41b8100185848ee
Nathan3-14 <nathan.watson.172@gmail.com>
2024-08-14 20:49:32 +0100

    Added repeating for dev_tools Added quit method for dev_tools

--- blog/dev_tools.py
+++ blog/dev_tools.py
@@ -1,13 +1,22 @@
+from pdb import run
 import app as a
 import pycmd as pc
+
+def quit() -> None:
+    global running
+    running = False
 
 reader = pc.CommandReader(
     [
         a.create_all,
         a.delete_user,
-        a.add_admin
+        a.add_admin,
+        quit
     ],
     "./dev_tools_help.yaml"
 )
 
-reader.prompt()
+running = True
+
+while running:
+    reader.prompt()
