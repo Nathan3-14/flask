fb71c34421675f34fc27fdd6ab3b8b65faf7296a
Nathan3-14 <nathan.watson.172@gmail.com>
2024-08-14 20:45:13 +0100

    Accounted for pycmd update

--- blog/dev_tools.py
+++ blog/dev_tools.py
@@ -1,16 +1,11 @@
 import app as a
 import pycmd as pc
 
-
-create_all_command = pc.Command(a.create_all)
-delete_user_command = pc.Command(a.delete_user)
-add_admin_command = pc.Command(a.add_admin)
-
 reader = pc.CommandReader(
     [
-        create_all_command,
-        delete_user_command,
-        add_admin_command
+        a.create_all,
+        a.delete_user,
+        a.add_admin
     ],
     "./dev_tools_help.yaml"
 )
